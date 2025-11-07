cd670381b83772412b39871968723f38fb2fd2ac
jinho4785 <jinho4785@gmail.com>
2025-11-07 18:33:20 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import EffectEx from "./EffectEx";
 import ContextEx from "./ContextEx";
 import ReducerEx from "./ReducerEx";
+import MemoEx from "./MemoEx";
 
 const App = () => {
   return (
     <div>
       {/* <EffectEx /> */}
       {/* <ContextEx /> */}
-      <ReducerEx />
+      {/* <ReducerEx /> */}
+      <MemoEx />
     </div>
   );
 }
